3db84ea208574ad9dcf637cc51a38567c40e4a36
fan9704 <cxz123499@gmail.com>
2022-03-25 21:10:07 +0800

    Complete User model

--- src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
+++ src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.bezkoder.spring.datajpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GarbageClassifierBackendApplicationTests {
+class SpringBootDataJpaApplicationTests {
 
 	@Test
 	void contextLoads() {
