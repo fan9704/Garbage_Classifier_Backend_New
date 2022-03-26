0b144c62be7a69a67fa08b86d933ce204af186cf
fan9704 <cxz123499@gmail.com>
2022-03-26 12:59:20 +0800

    Complete LoginController use Template

--- src/main/java/com/bezkoder/spring/datajpa/model/Tutorial.java
+++ src/main/java/com/bezkoder/spring/datajpa/model/Tutorial.java
@@ -1,7 +1,12 @@
 package com.bezkoder.spring.datajpa.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "tutorials")
 public class Tutorial {
