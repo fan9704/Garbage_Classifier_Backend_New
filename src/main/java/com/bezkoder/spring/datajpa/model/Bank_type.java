6257d0672787cef939411dd58b772c0044eeae94
fan9704 <cxz123499@gmail.com>
2022-03-28 00:41:59 +0800

    Create Model Bank_type

---
+++ src/main/java/com/bezkoder/spring/datajpa/model/Bank_type.java
@@ -0,0 +1,23 @@
+package com.bezkoder.spring.datajpa.model;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "Bank_type")
+public class Bank_type {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "bank_name")
+    private String bank_name;
+    @Column(name= "bank_code")
+    private int bank_code;
+}
