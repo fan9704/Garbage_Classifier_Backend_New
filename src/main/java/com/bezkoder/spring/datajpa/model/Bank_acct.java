b1c38a27cec770737c40fc8b6a0373b5a54b0354
fan9704 <cxz123499@gmail.com>
2022-03-28 14:03:19 +0800

    Add Bank_acct Model and Commit relationship to Bank_type

---
+++ src/main/java/com/bezkoder/spring/datajpa/model/Bank_acct.java
@@ -0,0 +1,34 @@
+package com.bezkoder.spring.datajpa.model;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "Bank_acct")
+public class Bank_acct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "bank_type_id")
+    private  Bank_type bank_type;
+
+//    @Column(name = "bank_type")
+//    private int bank_type;
+    @Column(name= "account_code")
+    private int account_code;
+
+    public Bank_acct(Bank_type bank_type,int account_code){
+        this.bank_type=bank_type;
+        this.account_code=account_code;
+    }
+
+}
