3e789361c5e5669759698384186fe8eabda6e949
kukina622 <wyes91622@gmail.com>
2022-04-01 03:02:30 +0800

    change Snake_Case to CamelCase in repository

--- src/main/java/com/bezkoder/spring/datajpa/repository/BankTypeRepository.java
+++ src/main/java/com/bezkoder/spring/datajpa/repository/BankTypeRepository.java
@@ -1,8 +1,11 @@
 package com.bezkoder.spring.datajpa.repository;
-
-import com.bezkoder.spring.datajpa.model.Transfer_money_record;
+import com.bezkoder.spring.datajpa.model.Bank_type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+
+
 @Repository
-public interface Transfer_money_recordRepository extends JpaRepository<Transfer_money_record, Long> {
+public interface BankTypeRepository extends JpaRepository<Bank_type, Long> {
+
 }
--- src/main/java/com/bezkoder/spring/datajpa/repository/TransferMoneyRecordRepository.java
+++ src/main/java/com/bezkoder/spring/datajpa/repository/TransferMoneyRecordRepository.java
@@ -1,11 +1,8 @@
 package com.bezkoder.spring.datajpa.repository;
-import com.bezkoder.spring.datajpa.model.Machine_storage;
+
+import com.bezkoder.spring.datajpa.model.Transfer_money_record;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-
-
 @Repository
-public interface Machine_storageRepository extends JpaRepository<Machine_storage, Long> {
-
+public interface TransferMoneyRecordRepository extends JpaRepository<Transfer_money_record, Long> {
 }
