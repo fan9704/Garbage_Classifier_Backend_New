1a15cd8bf419adf948c24136fadb99ef9809d45c
fan9704 <cxz123499@gmail.com>
2022-10-21 01:06:49 +0800

    Update TransferMoneyRecordController

--- src/main/java/com/bezkoder/spring/datajpa/controller/TransferMoneyRecordController.java
+++ src/main/java/com/bezkoder/spring/datajpa/controller/TransferMoneyRecordController.java
@@ -1,7 +1,7 @@
 package com.bezkoder.spring.datajpa.controller;
 
-import com.bezkoder.spring.datajpa.model.Transfer_money_record;
 import com.bezkoder.spring.datajpa.dto.Transfer_money_recordDTO;
+import com.bezkoder.spring.datajpa.model.Transfer_money_record;
 import com.bezkoder.spring.datajpa.service.TransferMoneyRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +21,7 @@ public class TransferMoneyRecordController {
     }
     @GetMapping("/transfer_money_record/{id}")
     public ResponseEntity<Transfer_money_record> getTransfer_money_recordById(@PathVariable("id") long id) {
-        return getTransfer_money_recordById(id);
+        return transfer_money_recordService.getTransfer_money_recordById(id);
     }
     @PostMapping("/transfer_money_record")
     public ResponseEntity<Transfer_money_record> createTransfer_money_record(@RequestBody Transfer_money_recordDTO transfer_money_recordDTO) {
