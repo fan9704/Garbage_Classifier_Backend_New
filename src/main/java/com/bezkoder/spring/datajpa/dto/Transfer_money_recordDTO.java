b3178c469baec12c3224d2107a1cf11e6678fea2
fan9704 <cxz123499@gmail.com>
2022-10-25 01:24:11 +0800

    Update Transfer_money_recordDTO with NoArgsConstructor

--- src/main/java/com/bezkoder/spring/datajpa/dto/Transfer_money_recordDTO.java
+++ src/main/java/com/bezkoder/spring/datajpa/dto/Transfer_money_recordDTO.java
@@ -1,17 +1,16 @@
 package com.bezkoder.spring.datajpa.dto;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
+import lombok.AllArgsConstructor;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
-import java.sql.Timestamp;
 
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Transfer_money_recordDTO {
     private  long receiver;
     private BigDecimal amount;
